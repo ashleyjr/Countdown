a7d561ec81fea998e6bf5ae53cccee2eda2ece87
Ashley J. Robinson <ashley181291@gmail.com>
2017-01-07 00:51:31 +0000

    Better op running

--- Countdown.py
+++ Countdown.py
@@ -1,34 +1,35 @@
 import sys
 
 
-def operation(op, one, two):
+def num_op(op, one, two):
     if 0 == op:
-        return 0
-    if 1 == op:
         return one + two
-    if 2 == op:
+    if 1 == op:
         return one - two
-    if 3 == op:
+    if 2 == op:
         return one * two
-    if 4 == op:
+    if 3 == op:
         return one / two
 
 
 def print_op(op):
     if 0 == op:
-        return ""
-    if 1 == op:
         return "+"
-    if 2 == op:
+    if 1 == op:
         return "-"
-    if 3 == op:
+    if 2 == op:
         return "*"
-    if 4 == op:
+    if 3 == op:
         return "/"
 
 
 def str_setup(ops, nums):
     print "(((((",
+    if ops[0]:
+        print
+
+
+
     for i in range(0, 5):
         print str(nums[i]) + " ) " + str(print_op(ops[i])) + " ",
     return str(nums[5])
@@ -43,19 +44,26 @@ def main():
         return
 
     """ Attempt to turn arguments in array of integers """
-    numbers = []
+    nums = []
     for i in range(1, 7):
         try:
-            numbers.append(int(sys.argv[i]))
+            nums.append(int(sys.argv[i]))
         except ValueError:
             print "All arguments must be numbers"
             return
 
-    operations = [1, 1, 1, 1, 1]
-    accumulator = numbers[0]
-    for i in range(0, 5):
-        accumulator = operation(operations[i], accumulator, numbers[i+1])
-    print str_setup(operations, numbers) + " = " + str(accumulator)
+    """ Run the hill climber """
+    ops = [0, 0, 0, 0, 0]
+    for length in range(2, 7):
+        for i in range(2, length):
+            sys.stdout.write("(")
+        accumulator = num_op(ops[0], nums[0], nums[1])
+        sys.stdout.write("(" + str(nums[0]) + print_op(ops[0]) + str(nums[1]) + ")")
+        for i in range(2, length):
+            accumulator = num_op(ops[i - 1], accumulator, nums[i])
+            sys.stdout.write(print_op(ops[0]) + str(nums[i]) + ")")
+        sys.stdout.write(" = " + str(accumulator) + "\n")
+
 
 if __name__ == "__main__":
     main()
