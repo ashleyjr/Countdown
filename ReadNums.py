0ce1e2f1decfc69aba2c3abfe29d9873f89c7ac0
Ashley J. Robinson <ashley181291@gmail.com>
2017-01-07 23:00:48 +0000

    Lots of training images for the number cards

--- ReadNums.py
+++ ReadNums.py
@@ -1,13 +1,47 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
+import os
+from PIL import Image
+from sklearn import datasets, svm, metrics
 
-img = cv2.imread('train1.jpg',0)
-edges = cv2.Canny(img,100,200)
+classifier = svm.SVC(gamma=0.001)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+scale = 50, 50
+threshold = 128
 
-plt.show()
+
+def load_im_array(path):
+    im = Image.open(path)
+    im = im.resize(scale, Image.ANTIALIAS).convert('L')
+    im = im.point(lambda x: 0 if x < threshold else 1, '1')
+    return list(im.getdata())
+
+
+""" Train the SVC """
+features = []
+samples = []
+training_path = 'TestImages/TrainingNumbers/'
+for i in range(1, 101):
+    j = 1
+    while True:
+        number = training_path + "/" + str(i) + "_" + str(j) +".png"
+        if os.path.isfile(number):
+            features.append(load_im_array(number))
+            samples.append(i)
+        else:
+            break
+        j += 1
+print features
+print samples
+classifier.fit(features, samples)
+
+
+""" Test the SVC """
+testing_path = 'TestImages/TestingNumbers/'
+for i in range(1, 101):
+    number = testing_path + "/" + str(i) + "_1.png"
+    if os.path.isfile(number):
+        output = classifier.predict(load_im_array(number))
+        if i == output:
+            print "PASS",
+        else:
+            print "FAIL",
+        print ": Image is " + str(i) + ", SVC returned " + str(output[0])
