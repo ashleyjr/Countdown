efa1cecbb4fbd98ce620a2b41a0d4534c2ad30bd
Ashley J. Robinson <ashley181291@gmail.com>
2017-01-07 00:19:06 +0000

    print the ops

--- Countdown.py
+++ Countdown.py
@@ -1,23 +1,61 @@
 import sys
 
 
+def operation(op, one, two):
+    if 0 == op:
+        return 0
+    if 1 == op:
+        return one + two
+    if 2 == op:
+        return one - two
+    if 3 == op:
+        return one * two
+    if 4 == op:
+        return one / two
+
+
+def print_op(op):
+    if 0 == op:
+        return ""
+    if 1 == op:
+        return "+"
+    if 2 == op:
+        return "-"
+    if 3 == op:
+        return "*"
+    if 4 == op:
+        return "/"
+
+
+def str_setup(ops, nums):
+    print "(((((",
+    for i in range(0, 5):
+        print str(nums[i]) + " ) " + str(print_op(ops[i])) + " ",
+    return str(nums[5])
+
+
 def main():
     """ Main """
 
     """ Check correct number of arguments """
-    if 6 >= len(sys.argv):
-        print "Requires 6 arguments"
+    if 7 != len(sys.argv):
+        print "Requires 6 numbers"
         return
 
     """ Attempt to turn arguments in array of integers """
     numbers = []
-    for arg in sys.argv:
+    for i in range(1, 7):
         try:
-            numbers.append(int(arg))
+            numbers.append(int(sys.argv[i]))
         except ValueError:
-            pass  # it was a string, not an int.
+            print "All arguments must be numbers"
+            return
 
-    print numbers
+    operations = [1, 1, 1, 1, 1]
+    accumulator = numbers[0]
+    for i in range(0, 5):
+        accumulator = operation(operations[i], accumulator, numbers[i+1])
+    print str_setup(operations, numbers) + " = " + str(accumulator)
 
 if __name__ == "__main__":
     main()
