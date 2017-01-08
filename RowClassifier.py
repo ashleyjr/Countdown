c29f84b67494c8c7b4714d0e9dcb911ea6127bcc
Ashley J. Robinson <ashley181291@gmail.com>
2017-01-08 01:30:44 +0000

    using opencv to do canny edge detection

--- RowClassifier.py
+++ RowClassifier.py
@@ -1,10 +1,10 @@
 import os
-from PIL import Image
+from PIL import Image, ImageFilter
 from sklearn import datasets, svm, metrics
 from sklearn.externals import joblib
 
 row_path = 'TestImages/TestingRows/'
-scale = 50, 50
+scale = 20, 20
 threshold = 128
 
 classifier = joblib.load('tile_classifier.pkl')
@@ -16,6 +16,7 @@ for f in os.listdir(row_path):
         crop_rectangle = ((step*i), 0, (step*(i+1)), height)
         cropped_im = im.crop(crop_rectangle)
         cropped_im = cropped_im.resize(scale, Image.ANTIALIAS).convert('L')
+        cropped_im = cropped_im.filter(ImageFilter.FIND_EDGES)
         cropped_im = cropped_im.point(lambda x: 0 if x < threshold else 255, '1')
         #cropped_im.show()
         print classifier.predict(list(cropped_im.getdata()))[0]
--- TrainTileClassifier.py
+++ TrainTileClassifier.py
@@ -1,5 +1,6 @@
 import os
-from PIL import Image
+import cv2
+from PIL import Image, ImageFilter
 from sklearn import svm
 from sklearn.externals import joblib
 
@@ -7,7 +8,7 @@ from sklearn.externals import joblib
 def main():
     filename = 'tile_classifier.pkl'
     training_path = 'TestImages/TrainingNumbers/'
-    scale = 50, 50
+    scale = 20, 20
     threshold = 128
     classifier = svm.SVC(gamma=0.001)
     features = []
@@ -17,9 +18,23 @@ def main():
         while True:
             number = training_path + "/" + str(i) + "_" + str(j) + ".png"
             if os.path.isfile(number):
+                img = cv2.imread(number, 0)
+                edges = cv2.Canny(img, 100, 200)
+                edges = cv2.resize(edges, (50, 50))
+                cv2.imwrite(training_path + "/canny_" + str(i) + "_" + str(j) + ".png", edges)
+            else:
+                break
+            j += 1
+    for i in range(1, 101):
+        j = 1
+        while True:
+            number = training_path + "/" + str(i) + "_" + str(j) + ".png"
+            if os.path.isfile(number):
                 im = Image.open(number)
                 im = im.resize(scale, Image.ANTIALIAS).convert('L')
+                im = im.filter(ImageFilter.FIND_EDGES)
                 im = im.point(lambda x: 0 if x < threshold else 255, '1')
+                #im.show()
                 features.append(list(im.getdata()))
                 samples.append(i)
             else:
