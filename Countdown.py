05c9a5fbbd1e3267e2c548865f98a5d6e8b86ca7
Ashley Robinson <ashley181291@gmail.com>
2017-01-20 23:39:57 +0000

    edge detection

--- Countdown.py
+++ Countdown.py
@@ -1,9 +1,12 @@
 import os
 import zipfile
+import cv2
 
 """ Global constants """
 data_zip = "data.zip"               # The zip archive
 clean_files = [".csv", ".jpg"]      # File extensions to clean
+data_file = "data.csv"
+img_ext = ".jpg"
 
 
 def unzip_data():
@@ -21,8 +24,27 @@ def clean_data():
             os.remove(f)
 
 
+def downscale_image(img, bottom, x, y):
+    """
+        Take bottom section of image
+        Rescale
+        Threshold around mean of image
+    """
+    #ret, img = cv2.threshold(img, img.mean(), 255, cv2.THRESH_BINARY)
+    #img = cv2.Canny(img,100,200)
+    width, height = tuple(img.shape[1::-1])
+    img = img[int(round((1 - bottom) * (height - 1))):(height - 1), 1:(width - 1)]
+    img = cv2.resize(img, (x, y))
+    img = cv2.Canny(img, 100, 200)
+    #ret, img = cv2.threshold(img, img.mean(), 255, cv2.THRESH_BINARY)
+    return img
+
+
 def main():
     unzip_data()
+    file_list = [f for f in os.listdir(".") if f.endswith(img_ext)]
+    for file in file_list:
+        cv2.imwrite(file, downscale_image(cv2.imread(file, 0), 0.2, 60, 20))
     clean_data()
 
 if __name__ == "__main__":
