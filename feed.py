4f72fe87e8b8b11ab42f5d97ef846c9c279907ae
Ashley J. Robinson <ashley181291@gmail.com>
2017-01-28 16:49:09 +0000

    resize and threshold the video feed

--- feed.py
+++ feed.py
@@ -7,6 +7,10 @@ import numpy as np
 from sklearn import svm
 from sklearn import cross_validation
 import matplotlib.pyplot as plt
+import shutil
+
+
+
 screenWidth, screenHeight = pyautogui.size()
 pyautogui.moveTo(screenWidth / 2, screenHeight / 2)
 pyautogui.moveTo(160,750)
@@ -18,11 +22,24 @@ pyautogui.press('a')
 pyautogui.keyUp('ctrl')
 pyautogui.typewrite('http://tvcatchup.com/watch/channel4')
 pyautogui.press('enter')
+if os.path.exists("feed"):
+    shutil.rmtree('feed')
+os.mkdir("feed")
 while(True):
-    name = time.strftime("%H_%M_%S") + ".png"
-    pyautogui.screenshot(name)
-    img = cv2.imread(name, 0)
+    timestamp = time.strftime("%H_%M_%S")
+    temp = timestamp + ".png"
+    pyautogui.screenshot(temp)
+    img = cv2.imread(temp, 1)
+    os.remove(temp)
     width, height = tuple(img.shape[1::-1])
-    img = img[int(height*0.2):int(height*0.8), int(width*0.2):int(width*0.8)]
-    cv2.imwrite(name, img)
+    img = img[int(height*0.31):int(height*0.775), int(width*0.26):int(width*0.73)]
+    cv2.imwrite("feed/" + timestamp + "_trigger.png", img)
+
+    img = cv2.imread("feed/" + timestamp + "_trigger.png", 0)
+    width, height = tuple(img.shape[1::-1])
+    img = img[int(round(0.75 * (height - 1))):(height - 1), 1:(width - 1)]
+    img = cv2.resize(img, (3, 12))
+    ret, img = cv2.threshold(img, img.mean(), 255, cv2.THRESH_BINARY)
+    img = cv2.resize(img, (300, 1200))
+    cv2.imwrite("feed/" + timestamp + "_down.png", img)
 
