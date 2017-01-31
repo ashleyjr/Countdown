22bf492fad2fbbf7336af1451b5d9f7c9fb365a5
Ashley J. Robinson <ashley181291@gmail.com>
2017-01-31 20:53:51 +0000

    saving feed files

--- feed.py
+++ feed.py
@@ -26,6 +26,7 @@ if os.path.exists("feed"):
     shutil.rmtree('feed')
 os.mkdir("feed")
 while(True):
+    """ Capture the image"""
     timestamp = time.strftime("%H_%M_%S")
     temp = timestamp + ".png"
     pyautogui.screenshot(temp)
@@ -33,13 +34,17 @@ while(True):
     os.remove(temp)
     width, height = tuple(img.shape[1::-1])
     img = img[int(height*0.31):int(height*0.775), int(width*0.26):int(width*0.73)]
-    cv2.imwrite("feed/" + timestamp + "_trigger.png", img)
+    cv2.imwrite("feed/" + timestamp + "_A_trigger.png", img)
 
-    img = cv2.imread("feed/" + timestamp + "_trigger.png", 0)
+    """ Take the bottom part of the image """
+    img = cv2.imread("feed/" + timestamp + "_A_trigger.png", 0)
     width, height = tuple(img.shape[1::-1])
-    img = img[int(round(0.75 * (height - 1))):(height - 1), 1:(width - 1)]
+    img = img[int(round(0.85 * (height - 1))):(height - 1), 1:(width - 1)]
+    cv2.imwrite("feed/" + timestamp + "_B_bottom.png", img)
+
+    """ Scale down the bottom part of the image """
     img = cv2.resize(img, (3, 12))
     ret, img = cv2.threshold(img, img.mean(), 255, cv2.THRESH_BINARY)
     img = cv2.resize(img, (300, 1200))
-    cv2.imwrite("feed/" + timestamp + "_down.png", img)
+    cv2.imwrite("feed/" + timestamp + "_C_downscale.png", img)
 
