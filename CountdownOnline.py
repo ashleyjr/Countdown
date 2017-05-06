0726f37fececf52d67fba16bf036aa4a0f096b92
Ashley J. Robinson <ashley181291@gmail.com>
2017-05-06 22:52:22 +0100

    still making linear

--- CountdownOnline.py
+++ CountdownOnline.py
@@ -1,11 +1,15 @@
 import time
 import cv2
 from feed import feed
+from still import still
 
 """ Constants """
 current = "current.png"
 
 def main():
+
+    """ Still handler """
+    s = still()
 
     """ Start the feed off """
     f = feed()
@@ -18,6 +22,12 @@ def main():
 
         """ Save the image to the disk """
         cv2.imwrite(current, im)
+
+        """ Load a new image """
+        s.load(current)
+
+        """ Classify """
+        print s.compress_make_linear()
 
 if __name__ == "__main__":
     main()
--- still.py
+++ still.py
@@ -1,5 +1,11 @@
 import cv2
 
+""" constants """
+x = 5
+y = 12
+bottom = 0.4
+
+
 class still():
     """
         Handle tasks related to the single frame
@@ -13,7 +19,7 @@ class still():
         """
             Use a path to load an image from the disk
         """
-
+        self.im = cv2.imread(path, 1)
         self.loaded = True
 
     def save(self, path):
@@ -25,6 +31,9 @@ class still():
         if not self.loaded:
             return
 
+        """ save the image to the disk """
+        cv2.imwrite(path, self.im)
+
     def compress_make_linear(self):
         """
             Prepare the image for the classifier
@@ -33,6 +42,20 @@ class still():
         """ Bail if no image loaded """
         if not self.loaded:
             return
+
+        """ Take bottom section"""
+        width, height = tuple(self.im.shape[1::-1])
+        im  = self.im[int(round((1 - bottom) * (height - 1))):(height - 1), 1:(width - 1)]
+
+        """ Scale down """
+        im = cv2.resize(im, (x, y))
+
+        """ Threshold against the mean """
+        ret, im = cv2.threshold(im, im.mean(), 255, cv2.THRESH_BINARY)
+
+        """ Return the flattened image """
+        return im.flatten()
+
 
     def segment(self):
         """
