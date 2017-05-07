8b0faffe56e150475bbe9dfc2eccf6c9606f4714
Ashley J. Robinson <ashley181291@gmail.com>
2017-05-07 12:53:08 +0100

    Edit the dataset as error in length

--- CountdownTrain.py
+++ CountdownTrain.py
@@ -6,8 +6,11 @@ def main():
         Begin training the classifiers using gathered training data
     """
 
+    """ Load all thr training data from know folder loaction """
     d = dataset()
-    d.load("data.zip")
+
+    print d.len()
+    print d.pattern(1)
 
 if __name__ == "__main__":
     main()
